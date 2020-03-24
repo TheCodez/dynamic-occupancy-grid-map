26e35379492c55a4d59a08d6f3726fee6565d40f
Michael Kösel <michaelkoesel@gmx.de>
2020-03-24 19:16:26 +0100

    Add ros project stub

---
+++ dogm_ros/include/dogm_ros/DOGMRosConverter.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace dogm 
+{
+
+class DOGMRosConverter
+{
+public:
+ 
+  DOGMRosConverter();
+
+  virtual ~DOGMRosConverter();
+};
+
+} /* namespace dogm */
---
+++ dogm_ros/src/DOGMRosConverter.cpp
@@ -0,0 +1,14 @@
+#include "dogm_ros/DOGMRosConverter.h"
+
+namespace dogm
+{
+
+DOGMRosConverter::DOGMRosConverter()
+{
+}
+
+DOGMRosConverter::~DOGMRosConverter()
+{
+}
+
+} /* namespace dogm */
