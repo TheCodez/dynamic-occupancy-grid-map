74acf5dd258128d29b113ebf0cea0e10ded7c368
c.bachhuber <c.bachhuber@samsung.com>
2020-04-13 16:01:16 +0200

    Refactor

--- dogm/demo/precision_evaluator.h
+++ dogm/demo/precision_evaluator.h
@@ -38,19 +38,22 @@ SOFTWARE.
 
 const float kMaximumAssignmentDistance = 5.0f;
 
+struct PointWithVelocity
+{
+    float x{0.0f};
+    float y{0.0f};
+    float v_x{0.0f};
+    float v_y{0.0f};
+};
+
 class PrecisionEvaluator
 {
 public:
     explicit PrecisionEvaluator(const SimulationData _sim_data, const float _resolution)
         : sim_data{_sim_data}, resolution{_resolution}
     {
-        cumulative_error_x_pos = 0.0f;
-        cumulative_error_y_pos = 0.0f;
-        cumulative_error_x_vel = 0.0f;
-        cumulative_error_y_vel = 0.0f;
         number_of_detections = 0;
-        number_of_missed_detections = 0;
-        // step_errors.reserve(sim_data.size());
+        number_of_unassigned_detections = 0;
     }
 
     void evaluateAndStoreStep(int simulation_step_index, const std::vector<Point<dogm::GridCell>>& cells_with_velocity,
@@ -65,30 +68,13 @@ public:
                 int cluster_id = iter.first;
                 std::vector<Point<dogm::GridCell>> cluster = iter.second;
 
-                float x_vel = 0.0f, y_vel = 0.0f, x_pos = 0.0f, y_pos = 0.0f;
-                for (auto& point : cluster)
-                {
-                    x_vel += point.data.mean_x_vel;
-                    y_vel += point.data.mean_y_vel;
-                    x_pos += point.x;
-                    y_pos += point.y;
-                }
-
-                float mean_x_vel = (x_vel / cluster.size()) * resolution;
-                float mean_y_vel = -(y_vel / cluster.size()) * resolution;
-                float mean_x_pos = (x_pos / cluster.size()) * resolution;
-                float mean_y_pos = (y_pos / cluster.size()) * resolution;
-
-                const float x_offset = 22.0f;                        // motivated by numerical experiments
-                const float y_offset = 2.0f * (25.0f - mean_y_pos);  // motivated by numerical experiments
-                mean_x_pos += x_offset;
-                mean_y_pos += y_offset;
+                PointWithVelocity cluster_mean = computeClusterMean(cluster);
 
                 std::vector<Vehicle> matching_groundtruth_vehicles{};
                 for (const auto& vehicle : groundtruth_vehicles)
                 {
-                    const float distance =
-                        sqrtf(powf(mean_x_pos - vehicle.pos[0], 2.0f) + powf(mean_y_pos - vehicle.pos[1], 2.0f));
+                    const float distance = sqrtf(powf(cluster_mean.x - vehicle.pos[0], 2.0f) +
+                                                 powf(cluster_mean.y - vehicle.pos[1], 2.0f));
                     if (distance < kMaximumAssignmentDistance)
                     {
                         matching_groundtruth_vehicles.push_back(vehicle);
@@ -97,35 +83,32 @@ public:
 
                 if (matching_groundtruth_vehicles.size() == 0)
                 {
-                    ++number_of_missed_detections;
+                    ++number_of_unassigned_detections;
                     continue;
                 }
 
                 std::sort(matching_groundtruth_vehicles.begin(), matching_groundtruth_vehicles.end(),
-                          [mean_x_pos, mean_y_pos](const Vehicle& a, const Vehicle& b) {
+                          [&cluster_mean](const Vehicle& a, const Vehicle& b) {
                               const float distance_a =
-                                  sqrtf(powf(mean_x_pos - a.pos[0], 2.0f) + powf(mean_y_pos - a.pos[1], 2.0f));
+                                  sqrtf(powf(cluster_mean.x - a.pos[0], 2.0f) + powf(cluster_mean.y - a.pos[1], 2.0f));
                               const float distance_b =
-                                  sqrtf(powf(mean_x_pos - b.pos[0], 2.0f) + powf(mean_y_pos - b.pos[1], 2.0f));
+                                  sqrtf(powf(cluster_mean.x - b.pos[0], 2.0f) + powf(cluster_mean.y - b.pos[1], 2.0f));
                               return distance_a < distance_b;
                           });
 
-                const auto vehicle = matching_groundtruth_vehicles[0];
+                const auto closest_vehicle = matching_groundtruth_vehicles[0];
+
+                accumulateErrors(cluster_mean, closest_vehicle);
 
                 if (print_current_precision)
                 {
                     std::cout << std::setprecision(2);
                     std::cout << "\nCluster ID=" << cluster_id << "\n";
-                    std::cout << "Vel. Err.: " << vehicle.vel[0] - mean_x_vel << " " << vehicle.vel[1] - mean_y_vel
-                              << ", Pos. Err.: " << vehicle.pos[0] - mean_x_pos << " " << vehicle.pos[1] - mean_y_pos
-                              << "\n";
+                    std::cout << "Vel. Err.: " << closest_vehicle.vel[0] - cluster_mean.v_x << " "
+                              << closest_vehicle.vel[1] - cluster_mean.v_y
+                              << ", Pos. Err.: " << closest_vehicle.pos[0] - cluster_mean.x << " "
+                              << closest_vehicle.pos[1] - cluster_mean.y << "\n";
                 }
-
-                cumulative_error_x_pos += std::abs(mean_x_pos - vehicle.pos[0]);
-                cumulative_error_y_pos += std::abs(mean_y_pos - vehicle.pos[1]);
-                cumulative_error_x_vel += std::abs(mean_x_vel - vehicle.vel[0]);
-                cumulative_error_y_vel += std::abs(mean_y_vel - vehicle.vel[1]);
-                ++number_of_detections;
             }
         }
     }
@@ -133,11 +116,11 @@ public:
     void printSummary()
     {
         std::cout << "\nMean absolute errors (x,y): \n";
-        std::cout << "Position: " << cumulative_error_x_pos / number_of_detections << " "
-                  << cumulative_error_y_pos / number_of_detections << "\n";
-        std::cout << "Velocity: " << cumulative_error_x_vel / number_of_detections << " "
-                  << cumulative_error_y_vel / number_of_detections << "\n\n";
-        std::cout << "Detections unassigned by evaluator: " << number_of_missed_detections << "\n";
+        std::cout << "Position: " << cumulative_error.x / number_of_detections << " "
+                  << cumulative_error.y / number_of_detections << "\n";
+        std::cout << "Velocity: " << cumulative_error.v_x / number_of_detections << " "
+                  << cumulative_error.v_y / number_of_detections << "\n\n";
+        std::cout << "Detections unassigned by evaluator: " << number_of_unassigned_detections << "\n";
         std::cout << "Maximum possible detections: " << sim_data[0].vehicles.size() * sim_data.size() << "\n";
     }
 
@@ -153,15 +136,48 @@ private:
         return dbscan.getClusteredPoints();
     }
 
+    void accumulateErrors(const PointWithVelocity& cluster_mean, const Vehicle& vehicle)
+    {
+        cumulative_error.x += std::abs(cluster_mean.x - vehicle.pos[0]);
+        cumulative_error.y += std::abs(cluster_mean.y - vehicle.pos[1]);
+        cumulative_error.v_x += std::abs(cluster_mean.v_x - vehicle.vel[0]);
+        cumulative_error.v_y += std::abs(cluster_mean.v_y - vehicle.vel[1]);
+        ++number_of_detections;
+    }
+
+    PointWithVelocity computeClusterMean(std::vector<Point<dogm::GridCell>>& cluster)
+    {
+        PointWithVelocity cluster_mean;
+        for (auto& point : cluster)
+        {
+            cluster_mean.x += point.x;
+            cluster_mean.y += point.y;
+            cluster_mean.v_x += point.data.mean_x_vel;
+            cluster_mean.v_y += point.data.mean_y_vel;
+        }
+
+        cluster_mean.x = (cluster_mean.x / cluster.size()) * resolution;
+        cluster_mean.y = (cluster_mean.y / cluster.size()) * resolution;
+        cluster_mean.v_x = (cluster_mean.v_x / cluster.size()) * resolution;
+        cluster_mean.v_y = -(cluster_mean.v_y / cluster.size()) * resolution;
+
+        const float x_offset = 22.0f;                            // motivated by numerical experiments
+        const float y_offset = 2.0f * (25.0f - cluster_mean.y);  // motivated by numerical experiments
+        cluster_mean.x += x_offset;
+        cluster_mean.y += y_offset;
+
+        return cluster_mean;
+    }
+
     SimulationData sim_data;
     float resolution;
-    float cumulative_error_x_pos;
-    float cumulative_error_y_pos;
-    float cumulative_error_x_vel;
-    float cumulative_error_y_vel;
+    // float cumulative_error_x_pos;
+    // float cumulative_error_y_pos;
+    // float cumulative_error_x_vel;
+    // float cumulative_error_y_vel;
+    PointWithVelocity cumulative_error;
     int number_of_detections;
-    int number_of_missed_detections;
-    // std::vector<?> step_errors;
+    int number_of_unassigned_detections;
 };
 
 #endif  // PRECISION_EVALUATOR_H
