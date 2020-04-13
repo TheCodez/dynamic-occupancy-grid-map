191fc262931d27b455eb14ede306f1a6b5ac0bb2
Christoph Bachhuber <c.bachhuber@samsung.com>
2020-04-13 12:48:35 +0200

    Fix simulator output coordinate system (#25)
    * Add coordinate system transform dependent on senor's fov
    
    * Nice visualization scenario
    
    * Improve naming

--- dogm/demo/main.cpp
+++ dogm/demo/main.cpp
@@ -106,13 +106,13 @@ int main(int argc, const char** argv)
 	auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
 	std::cout << "### DOGM initialization took: " << ms << " ms" << " ###" << std::endl << std::endl;
 
-	Simulator simulator(100);
-	simulator.addVehicle(Vehicle(6, glm::vec2(20, 10), glm::vec2(0, 0)));
+	Simulator simulator(100, laser_params.fov);
+	simulator.addVehicle(Vehicle(3, glm::vec2(30, 20), glm::vec2(0, 0)));
 //	simulator.addVehicle(Vehicle(5, glm::vec2(46, 20), glm::vec2(0, 20)));
 //	simulator.addVehicle(Vehicle(4, glm::vec2(80, 30), glm::vec2(0, -10)));
 
-	simulator.addVehicle(Vehicle(6, glm::vec2(40, 30), glm::vec2(20, 5)));
-	simulator.addVehicle(Vehicle(5, glm::vec2(80, 24), glm::vec2(-15, -5)));
+	simulator.addVehicle(Vehicle(6, glm::vec2(40, 35), glm::vec2(20, 0)));
+	simulator.addVehicle(Vehicle(5, glm::vec2(60, 24), glm::vec2(0, -10)));
 
 	float delta_time = 0.1f;
 	std::vector<std::vector<float>> sim_measurements = simulator.update(10, delta_time);
