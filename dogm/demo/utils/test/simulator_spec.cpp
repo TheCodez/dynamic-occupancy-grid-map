df948c74ff49b7e244c7ec0d6a0e1807321c1a40
Christoph Bachhuber <c.bachhuber@samsung.com>
2020-04-20 18:07:29 +0200

    Add gtest (#40)
    * Add gtest with toy example
    
    * Add util unit tests to CI
    
    * Implement suggestions from code review
    
    * Move unit tests to ubuntu build

---
+++ dogm/demo/utils/test/simulator_spec.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) 2020 Michael Koesel and respective contributors
+// SPDX-License-Identifier: MIT
+// See accompanying LICENSE file for detailed information
+
+#include "simulator.h"
+
+#include <glm/glm.hpp>
+#include <gtest/gtest.h>
+
+TEST(Vehicle, Constructor)
+{
+    const int width{3};
+    const glm::vec2 position{-10, 20};
+    const glm::vec2 velocity{15, -25};
+
+    Vehicle unit{width, position, velocity};
+
+    EXPECT_EQ(width, unit.width);
+    EXPECT_EQ(position, unit.pos);
+    EXPECT_EQ(velocity, unit.vel);
+}
