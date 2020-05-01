50604650f220ef4f10f717942785d34e670ef48a
c.bachhuber <c.bachhuber@samsung.com>
2020-05-01 17:31:57 +0200

    Add move tests

--- dogm/demo/utils/test/simulator_spec.cpp
+++ dogm/demo/utils/test/simulator_spec.cpp
@@ -7,14 +7,42 @@
 #include <glm/glm.hpp>
 #include <gtest/gtest.h>
 
-TEST(Vehicle, Constructor)
+class VehicleSpec : public ::testing::Test
 {
-    const int width{3};
-    const glm::vec2 position{-10, 20};
-    const glm::vec2 velocity{15, -25};
+protected:
+    const float m_width{3.5};
+    const glm::vec2 m_position{-10, 20};
+    const glm::vec2 m_velocity{5, -10};
+    Vehicle m_unit{m_width, m_position, m_velocity};
+};
 
-    Vehicle unit{width, position, velocity};
+TEST_F(VehicleSpec, Constructor)
+{
+    EXPECT_EQ(m_position, m_unit.pos);
+    EXPECT_EQ(m_velocity, m_unit.vel);
+}
 
-    EXPECT_EQ(position, unit.pos);
-    EXPECT_EQ(velocity, unit.vel);
+class VehicleSpecParametrized : public ::testing::TestWithParam<float>
+{
+protected:
+    const float m_width{3.5};
+    const glm::vec2 m_position{-12.3, 20.2};
+    const glm::vec2 m_velocity{5, -10};
+    Vehicle m_unit{m_width, m_position, m_velocity};
+};
+
+TEST_P(VehicleSpecParametrized, Move)
+{
+    const auto delta_time = GetParam();
+    const auto expected_position = m_position + delta_time * m_velocity;
+    m_unit.move(delta_time);
+    EXPECT_EQ(expected_position, m_unit.pos);
+    EXPECT_EQ(m_velocity, m_unit.vel);
+}
+
+INSTANTIATE_TEST_CASE_P(VehcileMoveTests, VehicleSpecParametrized, ::testing::Values(-3.2f, 0.0f, 3.4f, 10.5f));
+
+TEST_F(VehicleSpec, GetFacingSide)
+{
+    EXPECT_TRUE(true);
 }
