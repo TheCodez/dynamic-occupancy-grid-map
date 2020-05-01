47e6e73da22f1d4100cc00c1ffa90382a97368e6
c.bachhuber <c.bachhuber@samsung.com>
2020-05-01 17:56:59 +0200

    Test getFacingSide()

--- dogm/demo/utils/test/simulator_spec.cpp
+++ dogm/demo/utils/test/simulator_spec.cpp
@@ -6,6 +6,7 @@
 
 #include <glm/glm.hpp>
 #include <gtest/gtest.h>
+#include <vector>
 
 class VehicleSpec : public ::testing::Test
 {
@@ -20,6 +21,15 @@ TEST_F(VehicleSpec, Constructor)
 {
     EXPECT_EQ(m_position, m_unit.pos);
     EXPECT_EQ(m_velocity, m_unit.vel);
+}
+
+TEST_F(VehicleSpec, GetFacingSide)
+{
+    const float resolution = 1.5f;
+    const glm::vec2 leftmost_point = m_position + glm::vec2{-m_width * 0.5, 0.0f};
+    const std::vector<glm::vec2> expected_points{leftmost_point, leftmost_point + glm::vec2{resolution, 0.0f},
+                                                 leftmost_point + glm::vec2{2.0f * resolution, 0.0f}};
+    EXPECT_EQ(expected_points, m_unit.getPointsOnFacingSide(resolution));
 }
 
 class VehicleSpecParametrized : public ::testing::TestWithParam<float>
@@ -41,8 +51,3 @@ TEST_P(VehicleSpecParametrized, Move)
 }
 
 INSTANTIATE_TEST_CASE_P(VehcileMoveTests, VehicleSpecParametrized, ::testing::Values(-3.2f, 0.0f, 3.4f, 10.5f));
-
-TEST_F(VehicleSpec, GetFacingSide)
-{
-    EXPECT_TRUE(true);
-}
