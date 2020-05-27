bf21327e6269cdc74fce7d8dc057563aad45a504
Michael Kösel <michaelkoesel@gmx.de>
2020-05-27 12:31:18 +0200

    Fix linux build

--- dogm/demo/utils/metrics.cpp
+++ dogm/demo/utils/metrics.cpp
@@ -3,15 +3,16 @@
 // See accompanying LICENSE file for detailed information
 
 #include "metrics.h"
+#include <cmath>
 
 PointWithVelocity MAE::update(const PointWithVelocity& cluster_mean, const Vehicle& vehicle)
 {
     const auto error = computeError(cluster_mean, vehicle);
 
-    cumulative_error.x += std::abs(error.x);
-    cumulative_error.y += std::abs(error.y);
-    cumulative_error.v_x += std::abs(error.v_x);
-    cumulative_error.v_y += std::abs(error.v_y);
+    cumulative_error.x += abs(error.x);
+    cumulative_error.y += abs(error.y);
+    cumulative_error.v_x += abs(error.v_x);
+    cumulative_error.v_y += abs(error.v_y);
 
     ++number_of_detections;
 
@@ -33,10 +34,10 @@ PointWithVelocity RMSE::update(const PointWithVelocity& cluster_mean, const Vehi
 {
     const auto error = computeError(cluster_mean, vehicle);
 
-    cumulative_error.x += std::powf(error.x, 2.0f);
-    cumulative_error.y += std::powf(error.y, 2.0f);
-    cumulative_error.v_x += std::powf(error.v_x, 2.0f);
-    cumulative_error.v_y += std::powf(error.v_y, 2.0f);
+    cumulative_error.x += powf(error.x, 2.0f);
+    cumulative_error.y += powf(error.y, 2.0f);
+    cumulative_error.v_x += powf(error.v_x, 2.0f);
+    cumulative_error.v_y += powf(error.v_y, 2.0f);
 
     ++number_of_detections;
 
@@ -46,10 +47,10 @@ PointWithVelocity RMSE::update(const PointWithVelocity& cluster_mean, const Vehi
 PointWithVelocity RMSE::compute()
 {
     PointWithVelocity error;
-    error.x = std::sqrtf(cumulative_error.x / number_of_detections);
-    error.y = std::sqrtf(cumulative_error.y / number_of_detections);
-    error.v_x = std::sqrtf(cumulative_error.v_x / number_of_detections);
-    error.v_y = std::sqrtf(cumulative_error.v_y / number_of_detections);
+    error.x = sqrtf(cumulative_error.x / number_of_detections);
+    error.y = sqrtf(cumulative_error.y / number_of_detections);
+    error.v_x = sqrtf(cumulative_error.v_x / number_of_detections);
+    error.v_y = sqrtf(cumulative_error.v_y / number_of_detections);
 
     return error;
 }
