2b15d3e64d5be6d72eaeb4a143c35759612efd3a
Christoph Bachhuber <cbachhuber89@gmail.com>
2020-10-02 17:47:14 +0200

    Fix shellcheck issues from superlint

--- local_pipeline_ubuntu.sh
+++ local_pipeline_ubuntu.sh
@@ -17,11 +17,11 @@ while [[ $# -gt 0 ]]; do
         shift # past argument
         ;;
         -h|--help)
-        printf "Usage: $0 [-c] [-d] \n
+        printf "Usage: %s [-c] [-d] \n
 This scripts configures, builds, and executes the dogm library, unit tests, and the demo.
 You can use the following flags:\n
 -c      Clean build (removes previous build files)
--d      Debug build\n"
+-d      Debug build\n" $0
         exit 0
         ;;
         *)    # unknown option
@@ -32,15 +32,15 @@ You can use the following flags:\n
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
-find dogm/demo dogm/include dogm/src dogm/test -iname '*.h' -o -iname '*.cpp' -o -iname '*.cu' | xargs clang-format -i
+find dogm/demo dogm/include dogm/src dogm/test \( -iname '*.h' -o -iname '*.cpp' -o -iname '*.cu' \) -print0 | xargs -0 clang-format -i
 mkdir -p build && cd build
-if [ "$CLEAN_BUILD" = true ]; then rm -rf *; fi
+if [ "$CLEAN_BUILD" = true ]; then rm -rf ./*; fi
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON "${BUILD_DEBUG}" ../dogm
-make -j $(nproc)
+make -j "$(nproc)"
 
 # TODO: extend to check more/all .cpp (and .cu) files
 cd ..
-find -iname '*.cpp' | xargs clang-tidy -p build
+find -iname '*.cpp' -print0 | xargs -0 clang-tidy -p build
 
-ctest . -j $(nproc)
+ctest . -j "$(nproc)"
 ./demo/demo
